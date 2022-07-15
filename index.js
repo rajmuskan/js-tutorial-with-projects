feb7156291ec3910799d1d9902d90dbd0aa59d76
Muskan Raj <rajmuskam090@gmail.com>
2022-07-15 23:29:50 +0530

    index.js modified

--- index.js
+++ index.js
@@ -1,289 +1,302 @@
-//1.ways to print in JavaScript
-//console.log("Hello World");
-//alert("me");
-//document.write("this is document write");
+// 1. Ways to print in JavaScript
+// console.log("Hello World");
+// alert("me");
+// document.write("this is document write")
 
-//2.Javascript console API
+// 2. Javascript console API
+// console.log("Hello World", 4 + 6, "Another log");
+// console.warn("this is warning");
+// console.error("This is an error");
+
+// 3. JavaScript Variables
+// What are Variables? - Containers to store data values
 
 /*
-console.log("Hello World");
-console.warn("this is a warning");
-console.error("this is an error");
+multi 
+line 
+commment
 */
 
-//3. javaScript variables
-//Containers to store data values
 var number1 = 34;
 var number2 = 56;
-// console.log(number1+number2);
+// console.log(number1 + number2);
 
-//4. Data types in javaScript
-//Numbers
+// 4. Data types in JavaScript
+// Numbers
 var num1 = 455;
 var num2 = 56.76;
 
-//string
+// String
 var str1 = "This is a string";
-var str2 = "This is also a string";
+var str2 = 'This is also a string';
 
-//Objects
+// Objects
 var marks = {
-  ravi: 34,
-  shubham: 78,
-  harry: 99.977,
-};
+    ravi: 34,
+    shubham: 78,
+    harry: 99.977
+}
 // console.log(marks);
 
-//booleans
+// Booleans
 var a = true;
 var b = false;
-// console.log(a,b);
+// console.log(a, b);
 
-//var und = undefined;
+// var und = undefined;
 var und;
 // console.log(und);
 
 var n = null;
 // console.log(n);
-
 /*
-At a very high level, there are two types of datatypes in js
-1.Primitive data types: undefined, null, number, string, boolean, symbol
-2.Reference data types: arrays and objects
+At a very high level, there are two types of data types in JavaScript
+1. Primitive data types: undefined, null, number, string, boolean, symbol
+2. Reference data types: Arrays and Objects
 */
 
-var arr = [1, 2, "babu", 4, 5];
-// console.log(arr);
+var arr = [1, 2, "bablu", 4, 5]
+// console.log(arr)
 
-//operators
-//arithmetic operator
-//assignment operator
-//comparison operator
-//logical operator  &&, || ,!
+// Operators in JavaScript
+// Arithmetic Operators
+var a = 100;
+var b = 10;
+// console.log("The value of a + b is ", a+b);
+// console.log("The value of a - b is ", a-b);
+// console.log("The value of a * b is ", a*b);
+// console.log("The value of a / b is ", a/b);
 
-//Functions in js
-//DRY = Do not repeat yourself
+// Assignment Operators
+var c = b;
+// c += 2;
+// c -= 2; // c = c - 2;
+// c *= 2;
+// c /= 2;
+// console.log(c);
+
+// Comparison Operators
+var x = 34;
+var y = 56;
+// console.log(x == y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x > y);
+// console.log(x < y);
+
+// Logical Operators
+
+// Logical and
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+// Logical or
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// Logical not
+// console.log(!false);
+// console.log(!true);
+
+// Function in JavaScript
 function avg(a, b) {
-  c = (a + b) / 2;
-  return c;
+    c = (a + b) / 2;
+    return c;
 }
-
+// DRY = Do not repeat yourself
 c1 = avg(4, 6);
 c2 = avg(14, 16);
-// console.log(c1,c2);
+// console.log(c1, c2);
 
-//conditionals in js
-//if
-//if else
-//if else ladder
-
-//loops
-var arr = [1, 2, 3, 4, 5, 6, 7];
-//console.log(arr);
-
-/* 
-for (var i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-} 
-*/
-
-//another method for iterating in an array
-
+// Conditionals in JavaScript
 /*
-arr.forEach(function (element) {
-  console.log(element);
-});
-*/
-
-//while loop
-let j = 0;
-//const ac=0;
-
-/*
-while (j < arr.length) {
-  console.log(arr[j]);
-  j++;
+var age = 41;
+// Single if statement
+if(age > 18){
+    console.log('You can drink rasna water');
 }
+// if - else statement
+// if(age > 18){
+//     console.log('You can drink rasna water');
+// }
+// else{
+//     console.log('You cannot drink rasna water');
+// }
+
+age = 25;
+// if-else Ladder
+if(age > 32){
+    console.log("You are not a kid");
+}
+else if(age >26){
+    console.log("Bachhe nahi rahe");
+}
+else if(age >22){
+    console.log("Yes Bachhe nahi rahe");
+}
+else if(age >18){
+    console.log("18 Bachhe nahi rahe");
+}
+else{
+    console.log("Bachhe rahe");
+}
+console.log("End of ladder");
 */
 
-//dowhile loop
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+// for(var i=0;i<arr.length;i++){
+//     if(i==2){
+//         // break;
+//         continue;
+//     }
+//     console.log(arr[i])
+// }
 
-/*
-do {
-  console.log(arr[j]);
-  j++;
-} while (j < arr.length);
-*/
+// arr.forEach(function(element){
+//     console.log(element);
+// })
+// const ac = 0;
+// ac++;
+// ac = ac +1;
+// let j = 0;
+// while(j<arr.length){
+//     console.log(arr[j]);
+//     j ++;
+// }
 
-//break and continue
+// do{
+//     console.log(arr[j]);
+//     j++;
+// } while (j < arr.length);
 
-//Array methods
 let myArr = ["Fan", "Camera", 34, null, true];
-//console.log(myArr.length);
-myArr.pop(); //pops out last element.
-myArr.push("harry"); //insert this element in the last
-myArr.shift(); //removes first element
-const newLen = myArr.unshift("Harry"); //unshift will include this element in the first index in the array.
-//console.log(newLen);
-myArr.sort(); //sort the array
+// Array Methods
+// console.log(myArr.length);
+// myArr.pop();
+// myArr.push("harry")
+// myArr.shift()
+// const newLen = myArr.unshift("Harry")
+// console.log(newLen);
+// console.log(myArr);
 
-//string methods in js
-let string = "Harry is a good good boy";
-//console.log(string.length);
-//console.log(string.indexOf("good"));
-//console.log(string.lastIndexOf("good"));
-//console.log(string.slice(1,4));
-d = string.replace("Harry", "Rohan");
-//console.log(d, string);
+// String Methods in JavaScript
+let myLovelyString = "Harry is a good boy good good Harry";
+// console.log(myLovelyString.length)
+// console.log(myLovelyString.indexOf("good"))
+// console.log(myLovelyString.lastIndexOf("good"))
 
-//date 
+// console.log(myLovelyString.slice(1,4))
+d = myLovelyString.replace("Harry", "Rohan");
+// d = d.replace("good", "bad");
+// console.log(d, myLovelyString)
+
 let myDate = new Date();
-//console.log(myDate.getTime());
-//console.log(myDate.getFullYear());
-//console.log(myDate.getDay());
-//console.log(myDate.getMinutes());
-//console.log(myDate.getHours());
-
-//DOM - DOCUMENT OBJECT MODEL
-//DOM Manipulation
-
+// console.log(myDate.getTime());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDay());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getHours());
+ 
+// DOM Manipulation
 let elem = document.getElementById('click');
-//console.log(elem);
+// console.log(elem);
 
-let elemClass = document.getElementsByClassName('container');
-//console.log(elemClass);
-elemClass[0].style.background = "yellow";
-//console.log(elemClass[0].innerHTML);
-//console.log(elemClass[0].innerText);
+let elemClass = document.getElementsByClassName("container")
+// console.log(elemClass);
+// elemClass[0].style.background = "yellow";
+elemClass[0].classList.add("bg-primary")
+elemClass[0].classList.add("text-success")
+// console.log(elem.innerHTML);
+// console.log(elem.innerText); 
 
-tn = document.getElementsByTagName('div');
-//console.log(tn);
-createdElement= document.createElement('p');
+// console.log(elemClass[0].innerHTML);
+// console.log(elemClass[0].innerText); 
+tn = document.getElementsByTagName('div')
+// console.log(tn)
+createdElement = document.createElement('p');
 createdElement.innerText = "This is a created para";
 tn[0].appendChild(createdElement);
-//removeChild(element); --> removes an element
-//replaceChild(current element,prev element);  --> replaces the element
+createdElement2 = document.createElement('b');
+createdElement2.innerText = "This is a created bold";
+tn[0].replaceChild(createdElement2, createdElement);
+// removeChild(element); ---> removes an element
+ 
+// Selecting using Query
+// sel = document.querySelector('.container')
+// console.log(sel)
+// sel = document.querySelectorAll('.container')
+// console.log(sel)
 
-//Selecting using Query
-// sel = document.querySelector('.container');
-// console.log(sel);
-// sel = document.querySelectorAll('.container');
-// console.log(sel);
-
-//Events in JS
 // function clicked(){
-//     console.log('The button was clicked');
+//     console.log('The button was clicked')
 // }
-
 // window.onload = function(){
 //     console.log('The document was loaded')
-// }
 
+// }
+// Events in JavaScript
 // firstContainer.addEventListener('click', function(){
 //     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
-//     console.log("Container was clicked");
+//     console.log("Clicked on Container")
 // })
 
-// let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
 // firstContainer.addEventListener('mouseover', function(){
-//     document.querySelectorAll('.container')[1].innerHTML = prevHTML;
-//     console.log("Mouse on container");
+//     console.log("Mouse on Container")
 // })
 
 // firstContainer.addEventListener('mouseout', function(){
+//     console.log("Mouse out of Container");
+// })
+
+// let prevHTML = document.querySelectorAll('.container')[1].innerHTML;
+// firstContainer.addEventListener('mouseup', function(){
+//     document.querySelectorAll('.container')[1].innerHTML = prevHTML;
+//     console.log("Mouse up when clicked on Container");
+// })
+
+// firstContainer.addEventListener('mousedown', function(){
 //     document.querySelectorAll('.container')[1].innerHTML = "<b> We have clicked</b>"
-//     console.log("Mouse out of container");
+//     console.log("Mouse down when clicked on Container");
 // })
 
 
-//Arrow Functions
-
-// function summ(a,b){
+// Arrow Functions
+// function summ(a, b){
 //     return a+b;
 // }
-// another wayy
-
 summ = (a,b)=>{
     return a+b;
 }
 
 logKaro = ()=>{
-    document.querySelectorAll('.container')[1].innerHTML = "<b> Set Interval Fired </b>";
-
-    console.log("I am your log");
+    document.querySelectorAll('.container')[1].innerHTML = "<b> Set interval fired</b>"
+    console.log("I am your log")
 }
-// SetTimeout and setInterval
-//clr = setTimeout(logKaro, 5000);
-// logKaro is a function
-//clr = setInterval(logKaro, 2000);
-// we use clearInterval(clr)/clearTimeout(clr) to cancel setInterval/setTimeout.
+// SetTimeout and setinterval
+// clr = setTimeout(logKaro, 5000);
+// clr = setInterval(logKaro, 2000);
+// use clearInterval(clr)/clearTimeout(clr) to cancel setInterval/setTimeout
 
+// JavaScript localStorage
+// localStorage.setItem('name', 'harry') 
+// localStorage 
+// localStorage.getItem('name')
+// localStorage.removeItem('name')
+// localStorage.clear();
 
-//javaScript localStorage
-//data user k computer pe save krne me madad krti hai
-//key value pairs daaal skte hain local storage k andar
-// setItem aur getItem k help se
-//localStorage.setItem('name','harry')
-//localStorage
-//localStorage.getItem('name');
-//localStorage.removeItem('name')
-//localStorage.clear();
-
-
-//JSON - JavaScript object notation
-//data ko exchange krne me istemal kiya jata h
-//json sirf double quotes accept krta h
-//lekin objects single quotes v accept kr skta hai
-// obj = {name: "harry", length: 1, a: {this:'that'}}
+// Json 
+// obj = {name: "harry", length: 1, a: {this: 'tha"t'}}
 // jso = JSON.stringify(obj);
-// console.log(typeof jso);
-// console.log(jso);
+// console.log(typeof jso)
+// console.log(jso)
 // parsed = JSON.parse(`{"name":"harry","length":1,"a":{"this":"that"}}`)
 // console.log(parsed);
 
-//Templates literals - Bacticks
-a=34;
-console.log(`this is my ${a}`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Template literals - Backticks
+a = 34;
+console.log(`this is my ${a}`)
